ba24857c63207fcdec62212f0b2ac4d025d92440
cave <cave@caves-MacBook-Air.local>
2019-09-20 20:53:14 +0530

    skeletal contract

--- chaincodes/lib/certifier-contract.js
+++ chaincodes/lib/certifier-contract.js
@@ -1,18 +1,25 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
 
 class CertifierContract extends Contract {
-
+//genesis block
+    async initLedger(ctx) {
+        console.info('============= START : Initialize Ledger ===========');
+        const events = [];
+        for (let i = 0; i < eventId.length; i++) {
+            events[i].docType = 'event';
+            await ctx.stub.putState('event' + i, Buffer.from(JSON.stringify(events[i])));
+            console.info('Added <--> ', events[i]);
+        }
+        console.info('============= END : Initialize Ledger ===========');
+    }
+//check if certifier exists
     async certifierExists(ctx, certifierId) {
         const buffer = await ctx.stub.getState(certifierId);
         return (!!buffer && buffer.length > 0);
     }
-
+//creating certifer , if exissts - throw error 
     async createCertifier(ctx, certifierId, value) {
         const exists = await this.certifierExists(ctx, certifierId);
         if (exists) {
@@ -43,6 +50,15 @@ class CertifierContract extends Contract {
         await ctx.stub.putState(certifierId, buffer);
     }
 
+    async updateCertificate(ctx, employeeId, certifierId, ifVerified) {
+        if (!ifVerified) {
+            throw new Error(`The certificate for ${employeeId} could not be verified`);
+        }
+        const asset = JSON.parse(buffer.toString());
+        await ctx.stub.putState(certifierId,buffer);
+        return buffer = ctx.stub.getState(ifVerified);
+
+    }
     async deleteCertifier(ctx, certifierId) {
         const exists = await this.certifierExists(ctx, certifierId);
         if (!exists) {
@@ -50,6 +66,20 @@ class CertifierContract extends Contract {
         }
         await ctx.stub.deleteState(certifierId);
     }
+    async addEvent(ctx, eventName, id, expertise) {
+        console.info('============= START : Create event ===========');
+
+        const car = {
+            eventName,
+            docType: 'event',
+            id,
+            expertise,
+        };
+
+        await ctx.stub.putState(eventName, Buffer.from(JSON.stringify(event)));
+        console.info('============= END : Create event ===========');
+    }
+
 
 }
 
