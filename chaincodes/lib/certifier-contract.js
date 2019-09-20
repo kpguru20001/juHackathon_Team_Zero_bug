7bf7d95e69f6e0c0b83aa42d14d96a187fda8361
Anooppakki <anooppakki@gmail.com>
2019-09-21 00:42:28 +0530

    tested skeletal contracts

--- chaincodes/lib/certifier-contract.js
+++ chaincodes/lib/certifier-contract.js
@@ -1,3 +1,4 @@
+// role - 0 : certifier , 1 : employee, 2 : certificate, 3 : event
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
@@ -7,58 +8,77 @@ class CertifierContract extends Contract {
     async initLedger(ctx) {
         console.info('============= START : Initialize Ledger ===========');
         const events = [];
-        for (let i = 0; i < eventId.length; i++) {
+        for (let i = 0; i < events.length; i++) {
             events[i].docType = 'event';
             await ctx.stub.putState('event' + i, Buffer.from(JSON.stringify(events[i])));
-            console.info('Added <--> ', events[i]);
+            return('Added <--> ', events[i]);
         }
         console.info('============= END : Initialize Ledger ===========');
+        
     }
 //check if certifier exists
     async certifierExists(ctx, certifierId) {
         const buffer = await ctx.stub.getState(certifierId);
         return (!!buffer && buffer.length > 0);
     }
-//creating certifer , if exissts - throw error 
-    async createCertifier(ctx, certifierId, value) {
+//check if certificate exists
+    async certificateExists(ctx, certificateId) {
+        const buffer = await ctx.stub.getState(certificateId);
+        return (!!buffer && buffer.length > 0);
+    }
+//creating certifer , if exists - throw error , else taking the value and write it onto the chain
+    async createCertifier(ctx, certifierId,name) {
         const exists = await this.certifierExists(ctx, certifierId);
         if (exists) {
             throw new Error(`The certifier ${certifierId} already exists`);
         }
-        const asset = { value };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(certifierId, buffer);
+        const certif = {name:name,role:0};
+        const out = await ctx.stub.putState(certifierId,Buffer.from(JSON.stringify(certif)));
+        return out;
     }
-
+    async createCertificate(ctx, certifierId,certificateId,employeeId,desc) {
+        const exists = await this.certificateExists(ctx, certificateId);
+        if (exists) {
+            throw new Error(`The certificate ${certificateId} already exists`);
+        }
+        const certificate = {desc:desc,certifierId:certifierId,employeeId:employeeId,role:2,ifVerified:'Pending'};
+        const out = await ctx.stub.putState(certificateId,Buffer.from(JSON.stringify(certificate)));
+        return out;
+    }
+//reading the certifier, if doesnt exist throw erroe else takes the certifier id and return it
     async readCertifier(ctx, certifierId) {
-        const exists = await this.certifierExists(ctx, certifierId);
-        if (!exists) {
-            throw new Error(`The certifier ${certifierId} does not exist`);
+        const certifAsBytes = await ctx.stub.getState(certifierId); // get the car from chaincode state
+        if (!certifAsBytes || certifAsBytes.length === 0) {
+            throw new Error(`${certifierId} does not exist`);
         }
-        const buffer = await ctx.stub.getState(certifierId);
-        const asset = JSON.parse(buffer.toString());
-        return asset;
+        console.log(certifAsBytes.toString());
+        return certifAsBytes.toString();
     }
 
-    async updateCertifier(ctx, certifierId, newValue) {
-        const exists = await this.certifierExists(ctx, certifierId);
-        if (!exists) {
-            throw new Error(`The certifier ${certifierId} does not exist`);
+    async readCertificate(ctx, certificateId) {
+        const certificateAsBytes = await ctx.stub.getState(certificateId); // get the car from chaincode state
+        if (!certificateAsBytes || certificateAsBytes.length === 0) {
+            throw new Error(`${certificateId} does not exist`);
         }
-        const asset = { value: newValue };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(certifierId, buffer);
+        console.log(certificateAsBytes.toString());
+        return certificateAsBytes.toString();
     }
 
-    async updateCertificate(ctx, employeeId, certifierId, ifVerified) {
-        if (!ifVerified) {
-            throw new Error(`The certificate for ${employeeId} could not be verified`);
+//updating the certificate with the verification status, if not verified - throw error else wtite verified onto the chain
+    async updateCertificate(ctx, certificateId, ifVerified) {
+        console.info('============= START : changeCarOwner ===========');
+
+        const certificateAsBytes = await ctx.stub.getState(certificateId); // get the car from chaincode state
+        if (!certificateAsBytes || certificateAsBytes.length === 0) {
+            throw new Error(`${certificateId} does not exist`);
         }
-        const asset = JSON.parse(buffer.toString());
-        await ctx.stub.putState(certifierId,buffer);
-        return buffer = ctx.stub.getState(ifVerified);
+        const certificate = JSON.parse(certificateAsBytes.toString());
+        certificate.ifVerified = ifVerified;
+
+        return await ctx.stub.putState(certificateId, Buffer.from(JSON.stringify(certificate)));
 
     }
+//deleting the certifer, if doesnt throws and error else deletes the state  
     async deleteCertifier(ctx, certifierId) {
         const exists = await this.certifierExists(ctx, certifierId);
         if (!exists) {
@@ -66,18 +86,27 @@ class CertifierContract extends Contract {
         }
         await ctx.stub.deleteState(certifierId);
     }
-    async addEvent(ctx, eventName, id, expertise) {
+    async addEvent(ctx, eventId,eventName, expertise, certifierId) {
         console.info('============= START : Create event ===========');
 
-        const car = {
-            eventName,
-            docType: 'event',
-            id,
-            expertise,
+        const event = {
+            eventName:eventName,
+            role:3,
+            expertise:expertise,
+            certifierId:certifierId
         };
 
-        await ctx.stub.putState(eventName, Buffer.from(JSON.stringify(event)));
+        await ctx.stub.putState(eventId, Buffer.from(JSON.stringify(event)));
         console.info('============= END : Create event ===========');
+    }
+
+    async readEvent(ctx, eventId) {
+        const eventAsBytes = await ctx.stub.getState(eventId); // get the car from chaincode state
+        if (!eventAsBytes || eventAsBytes.length === 0) {
+            throw new Error(`${eventId} does not exist`);
+        }
+        console.log(eventAsBytes.toString());
+        return eventAsBytes.toString();
     }
 
 
